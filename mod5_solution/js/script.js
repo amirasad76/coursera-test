59e1b7a22e108ae1ea0371d29af1a518eab051c1
amirasad76 <72322886+amirasad76@users.noreply.github.com>
2020-11-22 22:29:37 +0330

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -119,8 +119,8 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-       console.log(html1);
-        // insertHtml("main-content",html1);
+       
+        insertHtml("#main-content",html1);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
